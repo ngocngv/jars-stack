0747c4d3b1bd21e803cfae210a8212f658ec5189
System <Mr.NN91@gmail.com>
2017-04-02 23:14:25 +0700

    Update NOTE.py

--- hdfs/NOTE.py
+++ hdfs/NOTE.py
@@ -2,4 +2,9 @@
 
 # https://github.com/TalkingData/magpie
 
+# https://github.com/ripboy/big-data-automation
+# https://github.com/futuresystems/big-data-stack
+
+
+# https://github.com/lizhitao0923/ansible-hadoop
 
